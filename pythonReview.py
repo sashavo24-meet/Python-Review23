4952c6a8c77ad4318b149d9469c94d3c4f861141
sashavo24-meet <avodrovnik@gmail.com>
2023-07-16 18:57:16 +0300

    Create pythonReview.py
    im still working on it

---
+++ pythonReview.py
@@ -0,0 +1,12 @@
+def create_youtube_video(title, description):
+	dictionary = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {}}
+	return dictionary
+
+
+def likes(youtube_video):
+	if "likes" in dictionary: 
+	    youtube_video["likes"]+=1
+
+	return dictionary
+
+print("bfksfdkskdf")
