70ff0652a6f371f0558a0ecadda2ef58bb487cd3
sashavo24-meet <avodrovnik@gmail.com>
2023-07-20 15:50:04 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,12 +1,18 @@
 def create_youtube_video(title, description):
-	dictionary = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {}}
-	return dictionary
-
+	youtube_video = {"title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {}}
+	return youtube_video
 
 def likes(youtube_video):
-	if "likes" in dictionary: 
+	if "likes" in youtube_video: 
 	    youtube_video["likes"]+=1
+	    return youtube_video
 
-	return dictionary
 
-print("bfksfdkskdf")
+def dislikes(youtube_video):
+	if "dislikes" in youtube_video: 
+	    youtube_video["dislikes"]+=1
+	    return youtube_video
+
+def add_comment(youtube_video, username, comment_text):
+	youtube_video['comments']['username'] = comment_text
+	return youtube_video
